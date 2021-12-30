2648f995bbfd26fb38a9c102192bdc8569b5b364
Corban Amouzou <c0dineDev@gmail.com>
2021-12-30 08:52:16 -0800

    Initial Commit

---
+++ Orion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Orion
+//
+//  Created by Corban Amouzou on 2021-12-30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
